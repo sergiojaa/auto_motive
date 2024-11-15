cef37ae8cc4fe85e071f3176aa86e66eb17cbf52
Sergi kaliashvili <kaliashvilisergi@gmail.com>
2024-11-16 03:10:04 +0400

    calculator design

--- src/components/Calculator.tsx
+++ src/components/Calculator.tsx
@@ -1,8 +1,13 @@
-
+import usaImage from '../images/usa.webp'
 export default function Calculator() {
   return (
-    <div className='mt-[130px]'>
-      <div> 
+    <div className='mt-[130px] flex flex-col items-center bg-cover bg-center justify-center '
+    style={{ backgroundImage: `url(${usaImage})` }}
+    >
+      <div className="bg-gray-500 m-[20px] w-[340px] flex flex-col items-center justify-center"
+      
+      >
+      <div className="flex flex-col justify-center items-center" > 
         <h2>ყურადღება</h2>
         <h2>კალკულატორი აჩვენებს საცალო ფასს,</h2>
         <h2>სადილერო პირობების გასაგებად მიმართეთ კომპანიას</h2>
@@ -10,7 +15,7 @@ export default function Calculator() {
       </div>
       <div>
         <h1>Auction</h1>
-        <select className="p-2 border rounded-md" onChange={(e) => console.log(e.target.value)}>
+        <select className="p-2 border rounded-md w-[280px] text-[20px] " onChange={(e) => console.log(e.target.value)}>
         <option value="">choose</option>
         <option value="Option 1">Copart</option>
         <option value="Option 2">IAAI</option>
@@ -26,10 +31,10 @@ export default function Calculator() {
       </div>
       <div>
         <h1>State / City</h1>
-        <select className="p-2 border rounded-md" onChange={(e) => console.log(e.target.value)}>
+        <select className="p-2 border rounded-md w-[280px] text-[20px]" onChange={(e) => console.log(e.target.value)}>
         <option value="">choose</option>
-        <option value="Option 1">Copart</option>
-        <option value="Option 2">IAAI</option>
+        <option value="Option 1">filadelfia</option>
+        <option value="Option 2">losangeles</option>
         <option value="Option 3">Manheim</option>
         <option value="Option 4">Adesa</option>
         <option value="Option 5">AuctionPipeline</option>
@@ -41,21 +46,40 @@ export default function Calculator() {
         </select>
       </div>
       <div>
-        <h1>State / City</h1>
-        <select className="p-2 border rounded-md" onChange={(e) => console.log(e.target.value)}>
+        <h1>USA port</h1>
+        <select className="p-2 border rounded-md w-[280px] text-[20px]" onChange={(e) => console.log(e.target.value)}>
         <option value="">choose</option>
-        <option value="Option 1">Copart</option>
-        <option value="Option 2">IAAI</option>
+        <option value="Option 1">filadelfia</option>
+        <option value="Option 2">losangeles</option>
         <option value="Option 3">Manheim</option>
         <option value="Option 4">Adesa</option>
         <option value="Option 5">AuctionPipeline</option>
         <option value="Option 6">Canada</option>
         <option value="Option 7">Sublot Copart</option>
         <option value="Option 8">Sublot IAAI</option>
-
+        
         
         </select>
       </div>
+      <div>
+        <h1>Destination</h1>
+        <select className="p-2 border rounded-md w-[280px] text-[20px] mb-[20px]" onChange={(e) => console.log(e.target.value)}>
+        <option value="">choose</option>
+        <option value="Option 1">filadelfia</option>
+        <option value="Option 2">losangeles</option>
+        <option value="Option 3">Manheim</option>
+        <option value="Option 4">Adesa</option>
+        <option value="Option 5">AuctionPipeline</option>
+        <option value="Option 6">Canada</option>
+        <option value="Option 7">Sublot Copart</option>
+        <option value="Option 8">Sublot IAAI</option>
+        
+        
+        </select>
+      </div>
+      </div>
+     
+
     </div>
   )
 }
